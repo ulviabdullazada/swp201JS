acc4d7ebfcf4549510a921536a16018c5a878e74
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-04-12 12:08:24 +0400

    conditions

---
+++ 2 - Conditions/assets/js/main.js
@@ -0,0 +1,101 @@
+// if(5 > 9){
+//     alert("bu duzdu");
+// }
+// else{
+//     alert("sehvdi");
+// }
+
+// var num = 5
+
+// if(num > 0){
+//     alert("musbetdir");
+// }
+// else if(num == 0){
+//     alert("ne menfidir ne musbet");
+// }
+// else if(num < 0){
+//     alert("menfidir")
+// }
+
+// alert(99/0);
+
+// let num = 0;
+
+// if(num > 0 || num % 2 == 0){
+//     alert(num);
+// }
+// else if(num < 0){
+//     alert(num * -1);
+// }
+// else{
+//     alert("Bu eded ishe yaramir");
+// }
+
+// var dayOfWeek = 6;
+
+// if (dayOfWeek == 1) {
+//     alert("Monday");
+// }
+// else if (dayOfWeek == 2) {
+//     alert("Tuesday");
+// }
+// else if (dayOfWeek == 3) {
+//     alert("Wendesday");
+// }
+// else if (dayOfWeek == 4) {
+//     alert("Thursday");
+// }
+// else if (dayOfWeek == 5) {
+//     alert("Friday");
+// }
+// else if (dayOfWeek == 6) {
+//     alert("Saturday");
+// }
+// else if (dayOfWeek == 7) {
+//     alert("Sunday");
+// }
+// else {
+//     alert("Wrong input");
+// }
+
+// switch (dayOfWeek) {
+//     case 1:
+//         alert("Monday");
+//         break;
+//     case 2:
+//         alert("Tuesday");
+//         break;
+//     case 3:
+//         alert("Wendesday");
+//         break;
+//     case 4:
+//         alert("Thursday");
+//         break;
+//     case 5:
+//         alert("Friday");
+//         break;
+//     case 6:
+//     case 7:
+//         alert("Weekend");
+//         break;
+//     default:
+//         alert("Wrong input");
+//         break;
+// }
+
+// var num = prompt("ededi daxil edin");
+
+// if (!isNaN(num)) {
+//     num % 2 == 0 ? alert("Cutdur") : alert("Tekdir")
+// }
+// else{
+//     alert("Wrong input");
+// }
+
+var name = prompt("adinizi daxil edin");
+if (name == "Sahib") {
+    alert("Olmaz");
+}
+else{
+    alert("Buyur kech");
+}
