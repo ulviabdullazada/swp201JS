fbd90469caf12eb3914bc8131635497278e0c08d
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-04-12 17:57:40 +0400

    loops

---
+++ 3 - Loops/assets/js/main.js
@@ -0,0 +1,56 @@
+// var s = 0;
+// var target = prompt("Ededi daxil edin");
+// if (!isNaN(target)) {
+//     for(var i = 1; i <= target; i++){
+//         s = s + i; 
+//     }
+//     console.log(s);
+// }
+// else{
+//     console.log("Adam kihsi olar");
+// }
+// s = 0 + 1 = 1
+// s = 1 + 2 = 3
+// s = 3 + 3 = 6
+// s = 6 + 4 = 10
+// s = 10 + 5 = 15
+
+// while(true){
+//     console.log("Salam");
+// }
+
+// var a = 22;
+// while(a > 10)
+// {
+//     console.log(a);
+//     a--;
+// }
+// var a = 5
+// while(a > 6){
+//     console.log("Salam");
+// }
+
+// do{
+//     console.log("Salam");
+// }
+// while(a > 6)
+// var num;
+// do{
+//     num = prompt("Ededi daxil edin")
+// }
+// while(isNaN(num))
+
+// alert(num % 2 == 0 ? "Cutdur":"Tekdir")
+// var num
+// num = prompt("Ededi daxil edin")
+// while(isNaN(num)){
+//     alert("Eded daxil et");
+//     num = prompt("Ededi daxil edin")
+// }
+
+for (let i = 0; i < 40; i++) {
+    if( i % 2 == 1){
+        continue;
+    } 
+    console.log(i)
+}
