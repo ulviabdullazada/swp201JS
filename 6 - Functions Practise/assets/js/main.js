5c86dc0bfeeec6eea06e452767fc17c066ea789a
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-04-18 16:11:09 +0400

    Functions practise

---
+++ 6 - Functions Practise/assets/js/main.js
@@ -0,0 +1,24 @@
+// function areaOfCircle(radius) {
+//     return 3 * radius * radius
+// }
+// console.log(areaOfCircle(prompt("Radiusu daxil et")));
+
+// function task2(number) {
+//     if (number >= 0) {
+//         var say = 1;
+//         while(number > 9){
+//             var tek = number % 10;
+//             number = (number-tek) / 10;
+//             say++;
+//         }
+//         console.log(say);
+//     }
+//     else{
+//         console.log("Wrong input");
+//     }
+// }
+// task2(0)
+
+
+//57 -> elli yeddi
+//1235 -> min iki yuz otuz bes
