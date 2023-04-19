096f1ecca4a27e596581621cb8f1109247680172
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-04-19 16:15:01 +0400

    Functions 2

--- 6 - Functions Practise/assets/js/main.js
+++ 6 - Functions Practise/assets/js/main.js
@@ -22,3 +22,45 @@
 
 //57 -> elli yeddi
 //1235 -> min iki yuz otuz bes
+
+// function findMin(arr){
+//     var min = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//         if (min > arr[i]) {
+//             min = arr[i]
+//         }
+//     }
+//     return min;
+// }
+
+// console.log(findMin([1,-2,5,8,9,-7,66]));
+
+// function sumEvenIndex(arr) {
+//     var sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i % 2 == 0) {
+//             sum += arr[i]
+//         }
+//     }
+//     return sum;
+// }
+// alert(sumEvenIndex([1,2,3,4,5]))
+
+function sumMinOddMaxEven(arr){
+    var min = arr[1];
+    var max = arr[0];
+    for (let i = 2; i < arr.length; i++) {
+        if (i % 2 == 0) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        else{
+            if (arr[i] < min) {
+                min=arr[i];   
+            }
+        }
+    }
+    return min+max;
+}
+alert(sumMinOddMaxEven([0,4,12,-2,4]))
