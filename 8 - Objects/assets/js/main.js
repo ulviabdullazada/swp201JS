2767e8110016b059a2ecf700e52fe374839e5c49
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-04-25 16:32:44 +0400

    hoisting

---
+++ 8 - Objects/assets/js/main.js
@@ -0,0 +1,88 @@
+// function funk() {
+//     var test = "Salam";
+//     return test;
+// }
+
+// console.log(funk());
+// console.log(test);
+// var test = "False";
+// if(true){
+//     var test = "True";
+//     console.log(test);
+// }
+// console.log(test);
+
+// let test = "Salam";
+// let test = "Aleykum"
+
+//hoisting
+// test = 123;
+// console.log(test);
+// var test;
+
+// a();
+
+// function a() {
+//     alert("Sahil Bulbule")
+// }
+
+// a();
+
+// var a = ()=>{
+//     alert("Sahil Salam");
+// }
+
+// const a = "Salam"
+// const b = "Aleykum";
+
+// console.log(a, b);
+
+// var person = {
+//     name: "Emil",
+//     surname : "Guliyev",
+//     age : 21
+// }
+
+// console.log(person);
+
+// function Student(ad, soyad, yash)
+// {
+//     this.name = ad,
+//     this.surname = soyad,
+//     this.age = yash,
+//     this.getInfo = function (){
+//         return this.name + " " + this.surname + " " + this.age
+//     }
+// }
+
+// let std1 = new Student("Ruslan","Vahabov",19);
+// let std2 = new Student("Saxib","Asad",20);
+
+// function getInfo(name, surname,age) {
+//     return name + " " + surname + " " + age
+// }
+// console.log(std1.getInfo());
+// console.log(std2);
+
+// Array.prototype.salam = function (){
+//     alert("Aleykum")
+// }
+// Array.prototype.uzunluq = Array.prototype.length;
+// console.log([1,2,3]);
+// let a = [1,13,20];
+// console.log(a);
+// a.salam();
+
+
+// console.log(new Student("Leyla","Huseynova",21));
+// console.log(std1);
+// console.log(std2);
+
+let obj = {
+    arr : [],
+    add : function(num){
+
+    }
+}
+obj.add(7)
+console.log(obj.arr);
