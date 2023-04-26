1addf4c1fa57135badbf118dc8663a56270a2c18
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-04-26 17:42:26 +0400

    string methods & homeworks

--- 9 - Array methods/assets/js/main.js
+++ 9 - Array methods/assets/js/main.js
@@ -8,10 +8,6 @@
 // let myArr = ["Banana", "Orange", "Apple", "Mango"];
 
 
-
-
-
-
 // console.log(myArr.sort(()=> 0.5-Math.random()));
 
 // console.log(myArr.slice(4,3));
@@ -68,3 +64,86 @@
 // },500)
 
 // console.log();
+
+
+// var str = "Salam, Dunya";
+
+// for(var i = 0; i < str.length; i++){
+//     console.log(str[i])
+// }
+
+// var newStr = str.slice(0); // (start, end)
+// var newStr = str.substring(2,4); // (start, end)
+// var newStr = str.substr(2,4); // (start, length)
+
+// var newStr = str.replace(/a/ig,"b");
+// var newStr = str.replaceAll("Dunya","Planet");
+// var newStr = str.toUpperCase();
+// var newStr = str.toLowerCase();
+
+// console.log(newStr);
+
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text1.concat("%", text2);
+
+// console.log(text3);
+
+// var str = "    Hello, World!    ";
+// console.log(str);
+// var newStr = str.trim();
+// console.log(newStr);
+
+// var newStr = str.trimStart();
+// var newStr = str.trimEnd();
+// console.log(newStr);
+
+// let text = "5";
+// let padded = text.padStart(3,"-");
+// let padded = padded.padEnd(5,"-");
+// console.log(padded);
+
+
+// var str = "Hello, World!";
+
+// console.log(str.charAt(0));
+// console.log(str.charCodeAt(1));
+
+// if(str.charCodeAt(0) === 72){
+//     console.log("Salam");
+// }
+
+// var str = "Hello, World!, sadfas , asd ";
+// var newStr= str.split(",");
+
+// console.log(newStr);
+
+// let text = "yusif.osmanovgmail.com";
+// let result = text.includes("@");
+
+// // console.log(result);
+
+// if(result === true){
+//     alert("Xosh gelmishsiniz!");
+// }
+// else{
+//     alert("Duzgun deyer yazin!");
+// }
+
+// Methoda ad gonderende adi ekrana cixardir(Sabir), ad ve soyad gonderende soyadi adi ekrana cixardir(Guliyev Sabir), ad,soyad ve ata adı göndərildikdə adin baş hərfi nöqtə soyad ata adi ekrana cixardirsiz(S.Guliyev Mehti). 
+
+// function FindInfo(name, surname, fatherName){
+
+//     if(name !== undefined && surname !== undefined && fatherName !== undefined) {
+//         alert(name[0] + "." + surname + " " + fatherName);
+//     }
+//    else if(name !== undefined && surname !== undefined && fatherName === undefined) {
+//         alert(name + " " + surname);
+//     }
+//     else if(name !== undefined && surname === undefined && fatherName === undefined) {
+//         alert(name);
+//     }
+
+// }
+
+// FindInfo("Tural", "Kalbaliyev");
