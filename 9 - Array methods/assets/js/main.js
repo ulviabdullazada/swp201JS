efe27e7dd25058e62ddcb12d776beeb91530ac42
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-04-26 16:24:30 +0400

    Array methods

---
+++ 9 - Array methods/assets/js/main.js
@@ -0,0 +1,70 @@
+// const myArr = [116,42,5,2,true,22,"Sahib"]
+
+
+// myArr.push("Nurlan");
+
+// console.log(myArr);
+
+// let myArr = ["Banana", "Orange", "Apple", "Mango"];
+
+
+
+
+
+
+// console.log(myArr.sort(()=> 0.5-Math.random()));
+
+// console.log(myArr.slice(4,3));
+
+// myArr.splice(0,0, "Elvin", "Nurlan","Vusal")
+
+// let stdns = ["Emil","Vusal","Kamil"]
+
+// console.log(myArr.concat(stdns,"Elnare","Sevinc",88));
+
+// delete myArr[0];
+// myArr.shift();
+
+// console.log(myArr.unshift("Emil"));
+// console.log(myArr);
+
+// myArr.push(17);
+// console.log(myArr);
+
+// console.log(myArr.pop());
+// console.log(myArr);
+
+// console.log(customJoin(myArr, "/"));
+
+// function customJoin(arr, sym=',') {
+//     let result = "";
+//     for (let i = 0; i < arr.length; i++) {
+//         result += arr[i] + sym;
+//     }
+//     return result;
+// }
+
+// console.log(myArr.toString());
+// console.log(custom2String(myArr));
+
+// function custom2String(arr) {
+//     let result = "";
+//     for (let i = 0; i < arr.length; i++) {
+//         result += arr[i] + ","
+//     }
+//     return result.slice(0,-1);
+// }
+
+// let words = "Hello world!!!";
+// let startIndex = 0;
+// setInterval(()=>{
+//     text.textContent  += words[startIndex];
+//     if (startIndex == words.length) {
+//         startIndex = -1;
+//         text.textContent = "";
+//     }
+//     startIndex++;
+//     // text.innerText = words.slice(0,-1)
+// },500)
+
+// console.log();
