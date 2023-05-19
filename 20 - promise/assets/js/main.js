df1296fc8da80be71b35fe400bb6a89219c41d88
Ulvi Abdullazada <ulvi.abdullazada@code.edu.az>
2023-05-19 16:54:11 +0400

    basket example

--- 20 - promise/assets/js/main.js
+++ 20 - promise/assets/js/main.js
@@ -18,8 +18,48 @@ fetched.then(res => res.json())
 .then(data => data.filter(d => d.name.includes('le')))
 .then(data=>{
     data.forEach(d=>{
-        list.innerHTML += '<li>' + d.name + '</li>'
+        list.innerHTML += '<button id='${d.id}>' + d.name + '</button>'
     })
 })
 .catch(e=>alert('Problem var'))
 
+
+// BASKET
+// function BasketItem(id,count){
+//     this.Id = id;
+//     this.Count = count;
+// }
+
+// let btns = document.querySelectorAll(".btn");
+
+// btns.forEach(btn=>{
+//     btn.addEventListener("click",function(){
+//         let dataId = Number(this.parentNode.parentNode.getAttribute("data-id"));
+//         console.log(dataId)
+
+//         let basket = localStorage.getItem("basket")
+//         if(!basket){
+//             basket = []
+//         }
+//         else{
+//             basket = JSON.parse(basket)
+//         }
+        
+//         let basketItem = basket.find(bi=>bi.Id == dataId)
+
+//         if(!basketItem){
+//             basketItem = new BasketItem(dataId,1)
+//             basket.push(basketItem);
+//         }
+//         else{
+//             basketItem.Count++;
+//         }
+
+//         localStorage.setItem("basket",JSON.stringify(basket))
+
+//         let badge = document.querySelector(".badge")
+
+//         badge.innerHTML = basket.length
+
+//     })
+// })
